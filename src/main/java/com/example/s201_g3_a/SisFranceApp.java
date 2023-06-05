3cb5cb2ba6ad529da3a1876660d32bff54c64bad
EnzoBEDOS22002844 <enzo.bedos@etu.univ-amu.fr>
2023-06-05 16:26:37 +0200

    new map

--- src/main/java/com/example/s201_g3_a/SisFranceApp.java
+++ src/main/java/com/example/s201_g3_a/SisFranceApp.java
@@ -11,7 +11,7 @@ public class SisFranceApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SisFranceApp.class.getResource("SisFrance-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("SisFranceApp");
         stage.setScene(scene);
         stage.show();
