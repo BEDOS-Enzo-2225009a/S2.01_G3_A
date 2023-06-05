fc144cdf9513d63f56544a015a88dd3c31c7d7b5
b22013685 <rayane.belabbas@etu.univ-amu.com>
2023-06-05 11:50:07 +0200

    rename 2

--- src/main/java/com/example/s201_g3_a/SisFranceApp.java
+++ src/main/java/com/example/s201_g3_a/SisFranceApp.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class SisFranceApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SisFranceApp.class.getResource("SisFrance-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("test function");
+        stage.setTitle("SisFranceApp");
         stage.setScene(scene);
         stage.show();
     }
