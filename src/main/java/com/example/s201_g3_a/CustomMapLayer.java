7c65b2676d030697acd21614d724d213c7f3985c
RayaneBELABBASB22013685 <rayanebelabbas15@gmail.com>
2023-06-11 23:51:13 +0200

    mouvement

--- src/main/java/com/example/s201_g3_a/CustomMapLayer.java
+++ src/main/java/com/example/s201_g3_a/CustomMapLayer.java
@@ -11,21 +11,21 @@ import java.util.ArrayList;
 
 public class CustomMapLayer extends MapLayer {
 
-
-        public void updateLayer(ArrayList<DonneesSismiques> listeSeismes){
-            clearLayer();
-            for(DonneesSismiques seisme : listeSeismes){
-                MapPoint point = new MapPoint(seisme.getLatitude(), seisme.getLongitude());
-                Point2D point2D = getMapPoint(point.getLatitude(),point.getLongitude());
-                Circle circle = new Circle(5, Color.RED);
-                circle.setCenterX(point2D.getX());
-                circle.setCenterY(point2D.getY());
-                this.getChildren().add(circle);
-                //oe oe
-            }
-        }
-
-        public void clearLayer(){
-            this.getChildren().clear();
-        }
+//
+//        public void updateLayer(ArrayList<DonneesSismiques> listeSeismes){
+//            clearLayer();
+//            for(DonneesSismiques seisme : listeSeismes){
+//                MapPoint point = new MapPoint(seisme.getLatitude(), seisme.getLongitude());
+//                Point2D point2D = getMapPoint(point.getLatitude(),point.getLongitude());
+//                Circle circle = new Circle(5, Color.RED);
+//                circle.setCenterX(point2D.getX());
+//                circle.setCenterY(point2D.getY());
+//                this.getChildren().add(circle);
+//                //oe oe
+//            }
+//        }
+//
+//        public void clearLayer(){
+//            this.getChildren().clear();
+//        }
 }
--- src/main/java/com/example/s201_g3_a/SisFranceView.java
+++ src/main/java/com/example/s201_g3_a/SisFranceView.java
@@ -1,6 +1,7 @@
 package com.example.s201_g3_a;
 
 
+import com.gluonhq.maps.MapLayer;
 import com.gluonhq.maps.MapPoint;
 import com.gluonhq.maps.MapView;
 import javafx.beans.property.ReadOnlyStringWrapper;
@@ -46,6 +47,11 @@ public class SisFranceView {
     public void setViewModel(SisFranceViewModel viewModel) {
         this.viewModel = viewModel;
 
+    }
+
+    private SisFranceApp MainApp;
+    public void setFranceApp(SisFranceApp MainApp) {
+        this.MainApp = MainApp;
     }
     @FXML
     private Menu carte;
@@ -156,92 +162,15 @@ public class SisFranceView {
 
 
     @FXML
-    private void initialize()
+    private void initialize() throws NullPointerException
     {
-        map.flyTo(0,pointFr,0.1);
 
-//        WebEngine engine = map.getEngine();
-//        engine.loadContent(
-//                "<!DOCTYPE html>\n"
-//                + "<html lang=\"fr\">\n"
-//                + "\n"
-//                + "<head>\n"
-//                + "    <meta charset=\"UTF-8\">\n"
-//                + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
-//                + "    <title>Carte</title>\n"
-//                + "    <!-- leafletjs CSS -->\n"
-//                + "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.css\" />\n"
-//                + "    <!-- leafletjs JS -->\n"
-//                + "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.js\"></script>\n"
-//                + "    <style>\n"
-//                + "        /* Carte plein écran */\n"
-//                + "        html,\n"
-//                + "        body {\n"
-//                + "            margin: 0;\n"
-//                + "            height: 100%;\n"
-//                + "        }\n"
-//                + "\n"
-//                + "        #map {\n"
-//                + "            width: 100%;\n"
-//                + "            height: 100%;\n"
-//                + "        }\n"
-//                + "    </style>\n"
-//                + "</head>\n"
-//                + "\n"
-//                + "<body>\n"
-//                + "\n"
-//                + "    <!-- L'endroit ou la carte va s'afficher -->\n"
-//                + "    <div id=\"map\"></div>\n"
-//                + "\n"
-//                + "    <script>\n"
-//                + "        /* Les options pour afficher la France */\n"
-//                + "        const mapOptions = {\n"
-//                + "            center: [46.225, 0.132],\n"
-//                + "            zoom: 6\n"
-//                + "        }\n"
-//                + "\n"
-//                + "        /* Les options pour affiner la localisation */\n"
-//                + "        const locationOptions = {\n"
-//                + "            maximumAge: 10000,\n"
-//                + "            timeout: 5000,\n"
-//                + "            enableHighAccuracy: true\n"
-//                + "        };\n"
-//                + "\n"
-//                + "        /* Création de la carte */\n"
-//                + "        var map = new L.map(\"map\", mapOptions);\n"
-//                + "\n"
-//                + "        /* Création de la couche OpenStreetMap */\n"
-//                + "        var layer = new L.TileLayer(\"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png\",\n"
-//                + "            { attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors' });\n"
-//                + "\n"
-//                + "        /* Ajoute la couche de la carte */\n"
-//                + "        map.addLayer(layer);\n"
-//                + "\n"
-//                + "        /* Verifie que le navigateur est compatible avec la géolocalisation */\n"
-//                + "        if (\"geolocation\" in navigator) {\n"
-//                + "            navigator.geolocation.getCurrentPosition(handleLocation, handleLocationError, locationOptions);\n"
-//                + "        } else {\n"
-//                + "            /* Le navigateur n'est pas compatible */\n"
-//                + "            alert(\"Géolocalisation indisponible\");\n"
-//                + "        }\n"
-//                + "\n"
-//                + "        function handleLocation(position) {\n"
-//                + "            /* Zoom avant de trouver la localisation */\n"
-//                + "            map.setZoom(16);\n"
-//                + "            /* Centre la carte sur la latitude et la longitude de la localisation de l'utilisateur */\n"
-//                + "            map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));\n"
-//                + "        }\n"
-//                + "\n"
-//                + "        function handleLocationError(msg) {\n"
-//                + "            alert(\"Erreur lors de la géolocalisation\");\n"
-//                + "        }\n"
-//                + "\n"
-//                + "    </script>\n"
-//                + "\n"
-//                + "</body>\n"
-//                + "\n"
-//                + "</html>");
-
+        if(map != null ) {
+            map.flyTo(0, pointFr, 0.1);
+        }
+        else {
+            System.out.println("");
+        }
 
         importer.setOnAction(event -> importerDonneesCsv());
         exporter.setOnAction(event -> exporterDonneesCsv());
@@ -407,34 +336,40 @@ public class SisFranceView {
 
 
 
+//    @FXML
+//    private void onCarteMenuchange() {
+//        if (carte.getText().equals("Graphique 1")) {
+//            carte.setText("Carte");
+//            graph1.setText("Graphique 1");
+//            graphPane.getChildren().clear(); // Efface le graphique précédent
+//            map.setVisible(true); // Affiche la carte
+//        } else if (carte.getText().equals("Graphique 2")) {
+//            carte.setText("Carte");
+//            graph2.setText("Graphique 2");
+//            graphPane.getChildren().clear(); // Efface le graphique précédent
+//            map.setVisible(true); // Affiche la carte
+//        } else if (carte.getText().equals("Graphique 3")) {
+//            carte.setText("Carte");
+//            graph3.setText("Graphique 3");
+//            graphPane.getChildren().clear(); // Efface le graphique précédent
+//            map.setVisible(true); // Affiche la carte
+//        } else {
+//            carte.setText("Graphique 1");
+//            graph1.setText("Carte");
+//            graph2.setText("Graphique 2");
+//            graph3.setText("Graphique 3");
+//            graphPane.getChildren().clear(); // Efface le graphique précédent
+//            map.setVisible(false); // Cache la carte
+//            afficherGraphique1(donneesSismiques); // Affiche le graphique 1 en utilisant les données pertinentes
+//
+//        }
+//    }
     @FXML
-    private void onCarteMenuchange() {
-        if (carte.getText().equals("Graphique 1")) {
-            carte.setText("Carte");
-            graph1.setText("Graphique 1");
-            graphPane.getChildren().clear(); // Efface le graphique précédent
-            map.setVisible(true); // Affiche la carte
-        } else if (carte.getText().equals("Graphique 2")) {
-            carte.setText("Carte");
-            graph2.setText("Graphique 2");
-            graphPane.getChildren().clear(); // Efface le graphique précédent
-            map.setVisible(true); // Affiche la carte
-        } else if (carte.getText().equals("Graphique 3")) {
-            carte.setText("Carte");
-            graph3.setText("Graphique 3");
-            graphPane.getChildren().clear(); // Efface le graphique précédent
-            map.setVisible(true); // Affiche la carte
-        } else {
-            carte.setText("Graphique 1");
-            graph1.setText("Carte");
-            graph2.setText("Graphique 2");
-            graph3.setText("Graphique 3");
-            graphPane.getChildren().clear(); // Efface le graphique précédent
-            map.setVisible(false); // Cache la carte
-            afficherGraphique1(donneesSismiques); // Affiche le graphique 1 en utilisant les données pertinentes
-
-        }
+    public void goToGraph1() throws IOException, InstantiationException, IllegalAccessException {
+        SisFranceApp.showScene("SisFrance-view-Graph.fxml");
     }
-
-
+    @FXML
+    public void goToCarte() throws IOException, InstantiationException, IllegalAccessException {
+        SisFranceApp.showScene("SisFrance-view.fxml");
+    }
 }
