122d60a83eb175f32fe6cc11d6bd9fa8524544e0
s22002518 <yoni.safran@etu.univ-amu.fr>
2023-06-12 16:07:03 +0200

    CSS bouton

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.s201_g3_a {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
